e6b340c2d767838a137bd95cb95ac86c8926e462
Aldian Kuncoro <92977244+Mass-Kunc@users.noreply.github.com>
2021-10-25 13:20:41 +0700

    Add files via upload

---
+++ function dengan module/module_keliling.py
@@ -0,0 +1,8 @@
+def kel_segitiga(sisi):
+    kel = 3*sisi
+    return kel
+
+
+def kel_persegi_pjg(panjang,lebar):
+    kel = (2*panjang)+(2*lebar)
+    return kel
---
+++ function dengan module/module_luas.py
@@ -0,0 +1,8 @@
+def luas_segi3(alas, tinggi):
+    luas = 0.5*alas*tinggi
+    return luas
+
+
+def luas_persegi_pjg(panjang, lebar):
+    luas = panjang*lebar
+    return luas
